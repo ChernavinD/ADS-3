9a991a04d12bd89d27cb30c85c04878503f246a4
Чернавин Данила <78860817+ChernavinD@users.noreply.github.com>
2021-05-09 18:03:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,7 +16,7 @@ return 3;
 
 std::string infx2pstfx(std::string inf) {
 TStack<char> stack1;
-std::string result = "";
+std::string result;
 for (int i = 0; i < inf.length(); i++) {
 if (inf[i] >= '0' && inf[i] <= '9') {
 while (inf[i] >= '0' && inf[i] <= '9') {
@@ -25,20 +25,20 @@ i++;
 }
 result += ' ';
 i--;
-}else if (stack1.isEmpty()){
+} else if (stack1.isEmpty()) {
 stack1.push(inf[i]);
-}else if (inf[i] == '('){
+} else if (inf[i] == '(') {
 stack1.push(inf[i]);
-}else if (prior(inf[i]) > prior(stack1.get())){
+} else if (prior(inf[i]) > prior(stack1.get())) {
 stack1.push(inf[i]);
-}else if (inf[i] == ')') {
+} else if (inf[i] == ')') {
 while (stack1.get() != '(') {
 result += stack1.get();
 result += ' ';
 stack1.pop();
 }
 stack1.pop();
-}else if (prior(inf[i]) <= prior(stack1.get())) {
+} else if (prior(inf[i]) <= prior(stack1.get())) {
 while (prior(inf[i]) <= prior(stack1.get())) {
 result += stack1.get();
 result += ' ';
@@ -60,16 +60,21 @@ return result;
 
 int eval(std::string pst) {
 TStack<int> stack2;
-std::string temp = "";
+char temp;
 int a, b;
 for (int i = 0; i < pst.length(); i++) {
 if (pst[i] >= '0' && pst[i] <= '9') {
 while (pst[i] >= '0' && pst[i] <= '9') {
-temp += pst[i];
-i++;
+int tempint = 0, decade = 1;
+for (i; isdigit(pst[i]); i++) {
+temp = pst[i];
+tempint *= decade;
+tempint += (int)temp - 48;
+decade *= 10;
 }
-stack2.push(atoi(temp.c_str()));
-temp = "";
+stack2.push(tempint);
+tempint = 0;
+}
 }else if (pst[i] != ' ') {
 b = stack2.get();
 stack2.pop();
@@ -78,13 +83,13 @@ stack2.pop();
 if (pst[i] == '+') {
 stack2.push(a + b);
 pst[i] = ' ';
-}else if (pst[i] == '-') {
+} else if (pst[i] == '-') {
 stack2.push(a - b);
 pst[i] = ' ';
-}else if (pst[i] == '*') {
+} else if (pst[i] == '*') {
 stack2.push(a * b);
 pst[i] = ' ';
-}else if (pst[i] == '/') {
+} else if (pst[i] == '/') {
 stack2.push(a / b);
 pst[i] = ' ';
 }
